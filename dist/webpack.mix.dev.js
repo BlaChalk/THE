1df1d45ce3e0d7660d5d11bd1542657e500bde06
shiouyan <black00731@gmail.com>
2020-04-05 01:14:24 +0800

    + app.sass

--- dist/webpack.mix.dev.js
+++ dist/webpack.mix.dev.js
@@ -1,5 +1,6 @@
-const mix = require('laravel-mix');
+"use strict";
 
+var mix = require('laravel-mix');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,6 +12,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/app.sass', 'public/css');
+
+mix.js('resources/js/app.js', 'public/js').sass('resources/sass/app.scss', 'public/css').sass('resources/sass/app.sass', 'public/css');
---
+++ dist/webpack.mix.prod.js
@@ -0,0 +1,1 @@
+"use strict";var mix=require("laravel-mix");mix.js("resources/js/app.js","public/js").sass("resources/sass/app.scss","public/css").sass("resources/sass/app.sass","public/css");
