b1cd897d4834bae7c8854954c08b540a07acf3d5
shiouyan <black00731@gmail.com>
2020-04-13 06:50:07 +0800

    + frontend layouts @view

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        @include('layouts.head')
+    </head>
+
+    <body>
+        <img class="BG1" src="/img/BGRight.png" alt="">
+        @include('layouts.preloader')
+
+        <div class="wrapper">
+
+            @include('layouts.header')
+
+            @yield('content')
+
+            @include('layouts.footer')
+
+        </div>
+
+        @include('layouts.js')
+
+        @yield('script')
+    </body>
+
+</html>
