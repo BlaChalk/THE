e9aaf572a8ef2a4f2cd7edc5cb138bfa8aac0265
Shiou-Yan <black00731@gmail.com>
2020-04-29 15:54:26 +0800

    fix nav bug

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,13 +1,17 @@
 <div class="header">
-    <a href="#">
+    <a href="/">
         <img class="logo" src="/img/logo.png" alt="THE">
     </a>
 
     <div class="nav">
-      <h3>HOME.</h3>
-      <h3>GAME.</h3>
-      <h3>ISSUE.</h3>
-      <h3>ABOUT US.</h3>
+      <a href="/">
+          HOME.
+      </a>
+      <a>GAME.</a>
+      <a>ISSUE.</a>
+      <a href="/about">
+          ABOUT US.
+      </a>
     </div>
 
 </div>
